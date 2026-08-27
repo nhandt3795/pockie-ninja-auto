07f161684a50251f0714629b688500aae739a2fb
nhandt <nhandt@ltsgroup.tech>
2026-08-27 19:27:19 +0700

    Fix slot machine over stone

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,20 +6,32 @@ async function clickIfVisible(locator, timeout = 5000) {
 }
 test.beforeEach(async ({ page }) => {
   await page.goto('https://pockieninja.online/')
+  await page.locator("div.button.start-button").click()
+  const [newWindow] = await Promise.all([
+  page.context().waitForEvent('page'),
+  await page.locator("//span[text()='Đăng nhập bằng Google']/ancestor::div[@role='button']").click()
+  ]);
   
+  await newWindow.locator("input#identifierId").fill(process.env.EMAIL)
+  await newWindow.locator("//span[text()='Next']/ancestor::button").click()
+  await newWindow.locator("input[name='Passwd']").fill(process.env.PASSWORD)
+  await newWindow.locator("//span[text()='Next']/ancestor::button").last().click()
+  await newWindow.close()
 })
-test.only('slot machine', async ({ page }) => {
+test('slot machine', async ({ page }) => {
   test.setTimeout(0);
   while (true) {
     await page.locator('button.slot-machine__challenge-btn.--default').click({timeout: 200000});
     await page.locator("//b[text()='Combat Results']/ancestor::div[@class='panel--dark']//button[text()='Close']").click({timeout: 200000});
     const notify = page.locator('//pre[contains(text(),"You obtained")]/ancestor::div[@class="panel--dark"]//button[text()="Close"]')
     await clickIfVisible(notify);
+    await page.locator("div.slot-machine__container > button.slot-machine__close-btn").click();
     await page.locator("//div[@data-text='Team Stone']/parent::div").click();
     await page.locator('//div[@class="clickable" and text()="Heal"]').click();
     await page.locator("//div[@data-text='Team Stone']/parent::div").click();
     await page.locator('//div[@class="clickable" and text()="Repair All"]').click();
     await page.locator("//div[@class='panel--dark']//button[text()='Accept']").click();
+    await page.locator("div.slot-machine__icon--dark > button").click();
   }
 });
 
